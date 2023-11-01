826a9bf4363a9e67fc82460d2d097e51ed2be9f3
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-01 16:14:52 -0400

    feat: add project files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{jsx,js}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: '#957DAD'
-      }
+        primary: '#957DAD',
+      },
     },
   },
   plugins: [],
-}
+};
